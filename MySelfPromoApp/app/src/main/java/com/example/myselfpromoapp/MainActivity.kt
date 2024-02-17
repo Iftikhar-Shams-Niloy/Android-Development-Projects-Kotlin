888ac4b2bf3e27124230c1f8475b3d734ebd885d
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-02-18 01:20:26 +0600

    *Updated*

--- MySelfPromoApp/app/src/main/java/com/example/myselfpromoapp/MainActivity.kt
+++ MySelfPromoApp/app/src/main/java/com/example/myselfpromoapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myselfpromoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val previewButton: Button = findViewById(R.id.button_preview)
+        previewButton.setOnClickListener{
+            onPreviewButtonClicked()
+        }
+    }
+
+    private fun onPreviewButtonClicked(){
+        val contactNameEditText : TextInputEditText = findViewById(R.id.edit_text_contact_name)
+        val contactNumberEditText : TextInputEditText = findViewById(R.id.edit_text_contact_number)
+        val testString = contactNameEditText.text.toString() + ", " +contactNumberEditText.text.toString() // Just Toasting a sample text when button clicked
+        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
     }
 }
