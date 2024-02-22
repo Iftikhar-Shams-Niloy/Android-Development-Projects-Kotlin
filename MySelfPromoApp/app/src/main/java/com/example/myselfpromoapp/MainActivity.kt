6554e22de2bafde84caddaad5e7f857a1cc46ee7
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-02-22 18:21:59 +0600

    *Updated*

--- MySelfPromoApp/app/src/main/java/com/example/myselfpromoapp/MainActivity.kt
+++ MySelfPromoApp/app/src/main/java/com/example/myselfpromoapp/MainActivity.kt
@@ -1,25 +1,68 @@
 package com.example.myselfpromoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Spinner
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var contactNameEditText: TextInputEditText // if I don't want it to be null: private var contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name)
+    lateinit var contactNumberEditText: TextInputEditText
+    lateinit var myDisplayNameEditText: TextInputEditText
+    lateinit var startDateEditText: TextInputEditText
+    lateinit var juniorCheckBox: CheckBox
+    lateinit var immediateStartCheckBox: CheckBox
+    lateinit var jobTitleSpinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val previewButton: Button = findViewById(R.id.button_preview)
+
+        viewFinder()
+
+        val previewButton = findViewById<Button>(R.id.button_preview)
         previewButton.setOnClickListener{
             onPreviewButtonClicked()
         }
+        // Putting spinner Values
+        val spinnerValues = arrayOf("Software Developer", "Data Analyst","Data Engineer", "Android Developer", "Android Engineer", "Web Developer", "Web Designer","Full Stack Developer") // val spinnerValues: Array<String> = arrayOf("Hello", "Hi", "Bye Bye")
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerValues) //val spinnerAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spinnerValues)
+        jobTitleSpinner?.adapter = spinnerAdapter
     }
-
+    private fun viewFinder(){
+        contactNameEditText = findViewById(R.id.edit_text_contact_name)
+        contactNumberEditText = findViewById(R.id.edit_text_contact_number)
+        myDisplayNameEditText = findViewById(R.id.edit_text_display_name)
+        startDateEditText = findViewById(R.id.edit_text_start_date)
+        juniorCheckBox = findViewById(R.id.check_box_junior)
+        immediateStartCheckBox = findViewById(R.id.check_box_immediate_start)
+        jobTitleSpinner = findViewById(R.id.spinner_job_title)
+    }
     private fun onPreviewButtonClicked(){
-        val contactNameEditText : TextInputEditText = findViewById(R.id.edit_text_contact_name)
-        val contactNumberEditText : TextInputEditText = findViewById(R.id.edit_text_contact_number)
-        val testString = contactNameEditText.text.toString() + ", " +contactNumberEditText.text.toString() // Just Toasting a sample text when button clicked
-        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
+        val contactName = contactNameEditText.text.toString()
+        val contactNumber = contactNumberEditText.text.toString()
+        val myDisplayName = myDisplayNameEditText.text.toString()
+        val includeJunior = juniorCheckBox.isChecked
+        val jobTitle = jobTitleSpinner.selectedItem.toString()
+        val immediateStart = immediateStartCheckBox.isChecked
+        val startDate = startDateEditText.text.toString()
+
+        val previewActivityIntent = Intent(this, PreviewActivity::class.java)
+
+        previewActivityIntent.putExtra("Contact_Name", contactName)
+        previewActivityIntent.putExtra("Contact_Number", contactNumber)
+        previewActivityIntent.putExtra("My_Display_Name", myDisplayName)
+        previewActivityIntent.putExtra("Include_Junior", includeJunior)
+        previewActivityIntent.putExtra("Job_Title", jobTitle)
+        previewActivityIntent.putExtra("Immediate_Start", immediateStart)
+        previewActivityIntent.putExtra("Start_Date", startDate)
+
+        startActivity(previewActivityIntent)
     }
 }
---
+++ MySelfPromoApp/app/src/main/java/com/example/myselfpromoapp/PreviewActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myselfpromoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Spinner
+import android.widget.TextView
+
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+        val contactName = intent.getStringExtra("Contact_Name")
+        val contactNumber = intent.getStringExtra("Contact_Number")
+        val myDisplayName = intent.getStringExtra("My_Display_Name")
+        val includeJunior = intent.getBooleanExtra("Include_Junior", false)
+        val jobTitle = intent.getStringExtra("Job_Title")
+        val immediateStart = intent.getBooleanExtra("Immediate_Start", false)
+        val startDate = intent.getStringExtra("Start_Date")
+
+        val myMessage = "Greetings,\n\n" +
+                "I am $contactName. I like to learn and always keep developing myself. I go by the name $myDisplayName.\n\n" +
+                "I worked as ${getMyJobTitle(includeJunior,jobTitle.toString())} in a reputed company.\n\n" +
+                "I have a portfolio of Android Applications, Web Applications and several Python Projects.\n\n" +
+                "If you have a position for me in your company you can contact me by $contactNumber.\n\n" +
+                "I prefer to start working from ${getMyStartDate(immediateStart,startDate.toString())}.\n\n" +
+                "Best regards,\n\n" +
+                "$contactName"
+
+        var textViewMessage : TextView = findViewById(R.id.text_view_message)
+        textViewMessage.text = myMessage
+    }
+    fun getMyJobTitle(includeJunior:Boolean, jobTitle:String) = if (includeJunior) "Junior $jobTitle" else jobTitle
+    fun getMyStartDate(immediateStart:Boolean, startDate:String) = if(immediateStart) "today" else startDate
+}
