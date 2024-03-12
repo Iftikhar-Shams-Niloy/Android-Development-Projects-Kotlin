f16da51a04d891fa82e0a8f87ec33e9ce7c6f1fb
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-03-12 22:41:56 +0600

    * Project Upadted *

--- MyAPIapp/app/build.gradle.kts
+++ MyAPIapp/app/build.gradle.kts
@@ -4,21 +4,17 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mydailyrecord"
+    namespace = "com.example.myapiapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.mydailyrecord"
+        applicationId = "com.example.myapiapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildFeatures{
-        viewBinding = true
     }
 
     buildTypes {
@@ -40,6 +36,7 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -47,5 +44,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
--- MyDailyRecord/app/src/main/java/com/example/mydailyrecord/MainActivity.kt
+++ MyDailyRecord/app/src/main/java/com/example/mydailyrecord/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.mydailyrecord
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.mydailyrecord.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.bottomNav
+
     }
 }
