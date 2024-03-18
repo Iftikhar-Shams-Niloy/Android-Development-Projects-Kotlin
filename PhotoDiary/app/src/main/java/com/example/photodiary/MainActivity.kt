65c51fc88319d14265419bffe74680e97ab51812
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-03-18 10:49:22 +0600

    * New Project Created *

---
+++ PhotoDiary/app/src/main/java/com/example/photodiary/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.photodiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navView : BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
+        navView.itemIconTintList = null
+    }
+}
